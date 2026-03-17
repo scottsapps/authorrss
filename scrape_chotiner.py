1b4c2e33af08fc66d1a46d44b2bb48f5ce36d9a2
scottsapps <scott@angstreich.net>
2026-03-16 21:52:19 -0400

    Update scrape_chotiner.py

--- scrape_chotiner.py
+++ scrape_chotiner.py
@@ -5,7 +5,7 @@ import xml.etree.ElementTree as ET
 import os
 
 URL = "https://www.newyorker.com/contributors/isaac-chotiner"
-FEED_FILE = "feed.xml"
+FEED_FILE = "chotiner-feed.xml"
 
 headers = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36"
